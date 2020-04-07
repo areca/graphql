5aec8a7c4b11f312eb08035e0749b5a06f5846a4
areca <kserin@gmail.com>
2020-04-07 18:41:26 +0300

    completed

---
+++ spacex-launch-stats/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const graphQLHTTP = require('express-graphql');
+const cors = require('cors');
+const schema = require('./schema');
+
+const app = express();
+
+// Allow cross-origin
+app.use(cors());
+
+app.use('/graphql', graphQLHTTP({
+      schema,
+      graphiql: true
+    })
+  );
+  
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
